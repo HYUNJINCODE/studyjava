90d44db22712e5e0c9a4508dd5396a7dd0dcbdda
LAPTOP-AJT7G1L5\hyunjin <leehj0110@kakao.com>
2020-10-31 17:42:38 +0900

    20-10-31

---
+++ src/shape/src/shape/Shape.java
@@ -0,0 +1,12 @@
+package shape;
+
+public abstract class Shape {
+    public String shapeName;
+
+    public Shape(String name) {
+        shapeName = name;
+    }
+
+    public abstract double computerArea();
+    public abstract double computerPerimeter();
+}
