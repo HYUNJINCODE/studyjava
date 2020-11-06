735cf672ac6cb999bb6ed17c7f04ebb5ad237051
LAPTOP-AJT7G1L5\hyunjin <leehj0110@kakao.com>
2020-11-06 12:18:37 +0900

    20-11-06

---
+++ src/learnjava/src/studyjava/MyHelloExam.java
@@ -0,0 +1,23 @@
+package studyjava;
+
+import java.lang.reflect.Method;
+
+public class MyHelloExam {
+    public static void main(String[] args) {
+        MyHello hello = new MyHello();
+
+        try {
+
+            Method method = hello.getClass().getDeclaredMethod("hello");
+            if(method.isAnnotationPresent(Count100.class)) {
+                for(int i =0; i<100; i++) {
+                    hello.hello();
+                }
+            }else {
+                hello.hello();
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+}
