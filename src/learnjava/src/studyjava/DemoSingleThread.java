fe3afe31ebb2542860f1d237e9eb4841da0eabbb
LAPTOP-AJT7G1L5\hyunjin <leehj0110@kakao.com>
2020-11-10 16:04:04 +0900

    20-11-10

--- src/learnjava/src/studyjava/DemoSingleThread.java
+++ src/learnjava/src/studyjava/DemoSingleThread.java
@@ -1,2 +1,10 @@
-package studyjava;public class DemoSingleThread {
+package studyjava;
+
+public class DemoSingleThread {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance("Foo");
+        Singleton anothersingleton = Singleton.getInstance("bar");
+        System.out.println(singleton.value);
+        System.out.println(anothersingleton.value);
+    }
 }
